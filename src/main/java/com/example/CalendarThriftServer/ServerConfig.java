baf88189716f1d026bdd4de8141bfe186c3201c5
tusharmelmalgi <tushar.melmalgi@capillarytech.com>
2022-07-27 12:11:54 +0530

    added beans to handle method servlet

--- src/main/java/com/example/CalendarThriftServer/ServerConfig.java
+++ src/main/java/com/example/CalendarThriftServer/ServerConfig.java
@@ -18,10 +18,56 @@ public class ServerConfig
         }
 
         @Bean
-        public ServletRegistrationBean thriftMeetingServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        public ServletRegistrationBean isAliveServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
             TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
 
             return new ServletRegistrationBean(tServlet, "/isAlive");
         }
+    @Bean
+    public ServletRegistrationBean cancelMeetingOfRemovedEmployeeServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/cancelMeetingOfRemovedEmployee");
+    }
+
+    @Bean
+    public ServletRegistrationBean updateStatusOfRemovedEmployeeServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/updateStatusOfRemovedEmployee");
+    }
+    @Bean
+    public ServletRegistrationBean checkEmployeeAvailabilityServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/checkEmployeeAvailability");
+    }
+    @Bean
+    public ServletRegistrationBean addMeetingDetailsServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/addMeetingDetails");
+    }
+
+    @Bean
+    public ServletRegistrationBean addEmployeeMeetingStatusServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/addEmployeeMeetingStatus");
+    }
+    @Bean
+    public ServletRegistrationBean findFreeMeetingRoomServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/findFreeMeetingRoom");
+    }
+
+    @Bean
+    public ServletRegistrationBean meetingRoomAvailablemServlet(TProtocolFactory protocolFactory, MeetingHandler handler) {
+        TServlet tServlet = new TServlet(new MeetingSvc.Processor<>(handler), protocolFactory);
+
+        return new ServletRegistrationBean(tServlet, "/meetingRoomAvailable");
+    }
+
 }
 
